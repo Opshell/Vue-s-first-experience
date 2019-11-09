2e478b5ffc8b3e1f33d6fcfc6715536a6f147bec
Opshell <phenomx9990@gmail.com>
2019-11-09 14:31:50 +0800

    v-for & v-bind(:) use.  ES6 ` 結合

--- js/first.js
+++ js/first.js
@@ -1,8 +1,20 @@
 new Vue({
     el: '#app',
+    delimiters: ['${', '}'],
     data: {
         msg: 'Hellow world!',
-        act: '',
-        todoList: ['一', 'two', 'Trois']
+        actValue: 0,
+        itemList: [
+            {name: 'opshell', value: 20},
+            {name: 'death', value: 23},
+            {name: 'adven', value: 456},
+            {name: 'bear', value: 229},
+            {name: 'kaven', value: 0},
+        ]
     }
 });
+
+/*
+    某些後臺程式會洗掉{{}}
+    所以可以用"delimiters"這個參數宣告別的判別符號(界定符)
+*/
