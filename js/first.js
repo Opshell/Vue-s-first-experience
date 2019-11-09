382269dcc3ffc5c099d495680849c10711dd2c92
Opshell <phenomx9990@gmail.com>
2019-11-09 15:56:05 +0800

    methods 補充

--- js/first.js
+++ js/first.js
@@ -26,5 +26,12 @@ const vm = new Vue({
     某些後臺程式會洗掉{{}}
     所以可以用"delimiters"這個參數宣告別的判別符號(界定符)
 
-    3. 雙向綁定v-model 結合 methods 的互動
+    3. 雙向綁定v-model 結合 methods 的互動{
+        那在寫 function 的時候要注意一點，
+        一定要用 function(){} 的方式，
+        這樣子裡面使用 this 才會指向 Vue 實例，
+        不要使用箭頭函式(arrow function)，
+        因為這樣 this 就不會是指向 Vue 實例，
+        就會沒辦法取到 Vue 實例其他的方法。
+    }
 */
